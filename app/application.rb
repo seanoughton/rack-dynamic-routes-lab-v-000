5ab1ef7b60e9341ebedb02e3716fafbc2e384e12
Sean Oughton <seanoughton@gmail.com>
2018-03-21 22:23:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path =="/items"
+    if req.path.match =="/items/"
       resp.write ""
     else
       resp.write "Route not found"
