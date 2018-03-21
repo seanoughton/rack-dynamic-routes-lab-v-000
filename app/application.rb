4478700cdcc80f309995fb0a029168327f255c70
Sean Oughton <seanoughton@gmail.com>
2018-03-21 22:38:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path=="/testing"
       resp.write "Route not found"
       resp.status = 404
-    elsif req.path.match(/songs/)
+    elsif req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find do |item|
         item.name == item_name
