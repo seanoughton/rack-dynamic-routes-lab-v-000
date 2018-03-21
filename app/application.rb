1c0e954dbf42b4a53cd617ec1e61b24b55a826bc
Sean Oughton <seanoughton@gmail.com>
2018-03-21 22:40:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,8 @@ class Application
     if req.path=="/testing"
       resp.write "Route not found"
       resp.status = 404
-    elsif req.path.match(/items/)
+    end
+    if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find do |item|
         item.name == item_name
